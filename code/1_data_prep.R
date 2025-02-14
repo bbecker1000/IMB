b73ae52578254e07bd984b8c4e279ff8ab2b6f99
Robin Hinks <robin.e.hinks@gmail.com>
2025-02-14 13:33:33 -0800

    cleaning data

--- code/1_data_prep.R
+++ code/1_data_prep.R
@@ -1,9 +1,12 @@
 library(dplyr)
 library(tidyverse)
 library(here)
+library(lubridate)
+library(forcats)
 
-setwd(here::here("Code"))
+setwd(here::here("code"))
 
+# reading in data, assigning data types
 raw_data <- read_csv(here::here("data", "IMB_RearingReleaseData.csv")) %>% 
   rename(imb_id = `IMB ID`,
          rearing_year = `REARING YEAR`,
@@ -37,11 +40,40 @@ raw_data <- read_csv(here::here("data", "IMB_RearingReleaseData.csv")) %>%
          survival = if_else((survival == "n" | survival == "NV"), "N", survival),
          sex = if_else(sex == "f", "F", sex),
          sex = if_else(sex == "m", "M", sex),
-         sex = if_else(sex == "UNK", NA, sex))
+         sex = if_else(sex == "UNK", NA, sex),
+         rearing_year = as.integer(rearing_year),
+         host_plant = as.factor(host_plant),
+         collection_site = as.factor(collection_site),
+         collection_area = as.factor(collection_area),
+         collection_stage = as.factor(collection_stage),
+         larval_days = as.integer(larval_days),
+         survival = as.factor(survival),
+         stage_at_death = as.factor(stage_at_death),
+         release_year = as.integer(release_year),
+         release_site = as.factor(release_site),
+         release_area = as.factor(release_area),
+         sex = as.factor(sex)) %>% 
+  mutate_at(vars(contains("date")), mdy)
+
+# removing illogical data
+cleaned_data <- raw_data %>% 
+  mutate(larval_days = if_else(larval_days < 0 | larval_days > 100, NA, larval_days)) %>% 
+  filter(!is.na(collection_date),
+         collection_date <= hatch_date,
+         hatch_date <= date_instar_2,
+         date_instar_2 <= date_instar_3,
+         date_instar_3 <= date_instar_4,
+         date_instar_4 <= date_instar_5,
+         date_instar_5 <= pupation_date)
 
 # getting a sense of NA values in each column
 
-raw_data %>% count(survival)
+raw_data %>% level(survival)
 raw_data %>% count(sex)
 raw_data %>% count(larval_days)
+
+raw_data %>% count(complete.cases(.))
+
+weird_data <- cleaned_data %>% 
+  filter(is.na(collection_date))
 
