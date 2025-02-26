52efc15cdf1ee1cd5ded44d4416ed6a25d4a4e71
Robin Hinks <robin.e.hinks@gmail.com>
2025-02-26 11:52:49 -0800

    working on flexsurv analysis

--- code/3_survival.R
+++ code/3_survival.R
@@ -6,6 +6,7 @@ library(forcats)
 library(ggplot2)
 library(survival)
 library(icenReg)
+library(flexsurv)
 
 data <- read_csv(here::here("data", "dd_data.csv")) %>% 
   mutate(
@@ -18,12 +19,61 @@ data <- read_csv(here::here("data", "dd_data.csv")) %>%
   ) %>% 
   mutate_at(vars(contains("stage")), 
             factor,
-            levels = c("egg", "first", "second", "third", "fourth", "fifth", "pupa"),
+            levels = c("first", "second", "third", "fourth", "fifth", "pupa"),
             ordered = TRUE) %>% 
   mutate(
-    event_type = if_else(survival == TRUE, 0, 3)
-  ) # need to make times numeric....
+    event = as.factor(if_else(survival, paste0("progression", (as.integer(stage) + 1)), "death")),
+    status = 1
+  ) %>% 
+  filter(duration > 0)
 
+write_csv(data, "data.csv")
+
+# need to add rows representing start times for adult and death
+# data2 <- data %>% 
+#   group_by(imb_id) %>%
+#   slice_max(stage) %>% 
+#   mutate( # 8 = death, 7 = adult, 9 = censored
+#     state = if_else(survival == FALSE, 8, if_else(state == 6, 7, 9)),
+#     start = end,
+#     end = NA
+#   )
+
+# trying to use flexsurv, hopefully will be able to just to one model
+
+fs.st1 <- flexsurvmix(Surv(duration, status) ~ 1, event = event,
+                    data = data %>% filter(stage == "first") %>% droplevels(.),
+                    dists = c(progression2 = "weibull", death = "exponential"))
+
+fs.st2 <- flexsurvmix(Surv(duration, status) ~ 1, event = event,
+                      data = data %>% filter(stage == "second") %>% droplevels(.),
+                      dists = c(progression3 = "weibull", death = "exponential"))
+
+fs.st3 <- flexsurvmix(Surv(duration, status) ~ 1, event = event,
+                      data = data %>% filter(stage == "third") %>% droplevels(.),
+                      dists = c(progression4 = "weibull", death = "exponential"))
+
+fs.st4 <- flexsurvmix(Surv(duration, status) ~ 1, event = event,
+                      data = data %>% filter(stage == "fourth") %>% droplevels(.),
+                      dists = c(progression5 = "weibull", death = "exponential"))
+
+fs.st5 <- flexsurvmix(Surv(duration, status) ~ 1, event = event,
+                      data = data %>% filter(stage == "fifth") %>% droplevels(.),
+                      dists = c(progression6 = "weibull", death = "exponential"))
+
+fs.stp <- flexsurvmix(Surv(duration, status) ~ 1, event = event,
+                      data = data %>% filter(stage == "pupa") %>% droplevels(.),
+                      dists = c(progression7 = "weibull", death = "exponential"))
+
+fs.total <- fmixmsm("1st instar" = fs.st1, "2nd instar" = fs.st2, "3rd instar" = fs.st3)
+
+ppath_fmixmsm(fs.total, B = 100)
+
+qfinal_fmixmsm(fs.total, B = 10)
+
+ajfit_flexsurvmix(fs.st2, startname = "second")
+
+# trying to use icenReg, requires separate models for each state
 stage1 <- data %>% 
   filter(stage == "first")
 
